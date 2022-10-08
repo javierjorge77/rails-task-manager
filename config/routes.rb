aab586f89bc323d739df944c8d38c4e0111aa9d5
jorge perez <javierjorge77@gmail.com>
2022-10-08 15:26:51 -0500

    feo pero funcional

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,23 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+
+  #########          READ        #########
+  root "tasks#index"
+  get "tasks", to: "tasks#index"
+
+  #########          CREATE        #########
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+
+  #########          UPDATE        #########
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
+
+  #########          DESTROY        #########
+  delete "tasks/:id", to: "tasks#destroy"
+
+  #########          SHOW BY ID        #########
+  get "tasks/:id", to: "tasks#show", as: :task
+
 end
